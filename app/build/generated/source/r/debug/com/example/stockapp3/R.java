715aaff45101e679bd96afa4141db7873dff3827
Albert Setiawan <albert.setiawan98@hotmail.com>
2018-09-30 16:43:57 +0700

    layout dummy jadi

--- app/build/generated/source/r/debug/com/example/stockapp3/R.java
+++ app/build/generated/source/r/debug/com/example/stockapp3/R.java
@@ -18,12 +18,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
+        public static final int action_settings=0x7f080003;
         public static final int btnClear=0x7f080001;
-        public static final int btnPPCalc=0x7f080004;
-        public static final int btnPPClose=0x7f080005;
-        public static final int btnShow=0x7f080002;
-        public static final int displayData=0x7f080003;
+        public static final int displayData=0x7f080002;
         public static final int insertSymbol=0x7f080000;
     }
     public static final class layout {
