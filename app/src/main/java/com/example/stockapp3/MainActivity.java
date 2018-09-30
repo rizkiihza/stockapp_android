8bb9b6223e392e6fb20bfd4e151cb0f67b794551
rizkiihza <rizki.ihza@gmail.com>
2018-09-30 17:22:25 +0700

    done close

--- app/src/main/java/com/example/stockapp3/MainActivity.java
+++ app/src/main/java/com/example/stockapp3/MainActivity.java
@@ -10,6 +10,7 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Iterator;
 import java.util.List;
 import java.util.TimeZone;
 
@@ -18,6 +19,7 @@ import yahoofinance.*;
 import yahoofinance.histquotes.HistoricalQuote;
 import yahoofinance.histquotes.Interval;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.app.Activity;
@@ -30,26 +32,95 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends Activity {
 
-	List<BigDecimal> historical_quote = new ArrayList<BigDecimal>();
-	List<BigDecimal> historical_low = new ArrayList<BigDecimal>();
-	List<BigDecimal> historical_high = new ArrayList<BigDecimal>();
 	MACD macd = new MACD();
 	RSI cekRSI = new RSI();
 
 	Button btnShow, btnClear;
+
+	List<Double> s_close = new ArrayList<Double>();
+	List<Double> s_high = new ArrayList<Double>();
+	List<Double> s_low = new ArrayList<Double>();
+    List<String> s_date = new ArrayList<String>();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 
+        StrictMode.setThreadPolicy(policy);
+
+        btnShow = (Button) findViewById(R.id.btnShow);
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                get_result("BBNI");
+            }
+        });
 	}
 
 
-	private void get_price(Stock stock_name) {
+	private void get_result(String stock_name) {
+	    s_close.clear();
+	    s_high.clear();
+	    s_low.clear();
+	    s_date.clear();
 
+        new StockAppResult().execute(stock_name);
 	}
+
+    class StockAppResult extends AsyncTask<String, Void, JSONObject> {
+
+        private Exception exception;
+
+        protected JSONObject doInBackground(String... stock_name) {
+            String url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + stock_name[0] + ".JK&apikey=E5FDVTPST51Z1DXC";
+            JSONObject response = new JSONObject();
+
+            try{
+                Log.d("Get Price", stock_name[0]);
+                response = Utils.getJSONObjectFromURL(url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return response;
+        }
+
+        protected void onPostExecute(JSONObject jsonResult) {
+            // TODO: check this.exception
+            // TODO: do something with the feed
+            try {
+                JSONObject timeSeries = jsonResult.getJSONObject("Time Series (Daily)");
+
+                for(Iterator<String> iter = timeSeries.keys(); iter.hasNext();) {
+                    String key = iter.next();
+
+                    JSONObject price = timeSeries.getJSONObject(key);
+
+                    // handle per kategori
+                    Object close = price.get("4. close");
+                    Object high = price.get("2. high");
+                    Object low = price.get("3. low");
+
+                    s_close.add(0, Double.parseDouble(close.toString()));
+                    s_high.add(0, Double.parseDouble(high.toString()));
+                    s_low.add(0, Double.parseDouble(low.toString()));
+                    s_date.add(0, key);
+                }
+
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
